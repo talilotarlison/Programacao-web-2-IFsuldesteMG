0b77d2c9beddafaef8bd3453656a0128e67edc4a
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2025-09-30 18:41:32 -0300

    maior ou menor

---
+++ exercicios-aula/ex16.php
@@ -0,0 +1,13 @@
+<?php
+    $numero1 = 5;
+    $numero2 = 6;
+
+    // Verificando se $numero1 é maior ou menor que $numero2
+    if ($numero1 > $numero2) {
+        echo "{$numero1} é maior que {$numero2}.<br>";
+    } elseif ($numero1 < $numero2) {
+        echo "{$numero1} é menor que {$numero2}.<br>";
+    } else {
+        echo "{$numero1} é igual a {$numero2}.<br>";
+    }
+?>
