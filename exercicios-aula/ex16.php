5ee4a8d1fe9f67e0c413118fefe86307be6ac288
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2025-09-30 18:43:20 -0300

    menor maior

--- exercicios-aula/ex16.php
+++ exercicios-aula/ex16.php
@@ -2,6 +2,9 @@
     $numero1 = 5;
     $numero2 = 6;
 
+    // Para verificar se $numero1 é maior ou menor que $numero2, você pode usar os operadores de comparação (>, <, >=, <=, ==, !=). 
+    // Neste caso, você deseja comparar se $numero1 é maior ou menor que $numero2. Vou usar a estrutura if-else para isso.
+
     // Verificando se $numero1 é maior ou menor que $numero2
     if ($numero1 > $numero2) {
         echo "{$numero1} é maior que {$numero2}.<br>";
