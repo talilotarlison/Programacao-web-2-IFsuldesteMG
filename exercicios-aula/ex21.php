946145aefc294b4c67caea43a40e4a00893b8ce5
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2025-10-01 12:21:10 -0300

    exercicio

--- exercicios-aula/ex21.php
+++ exercicios-aula/ex21.php
@@ -1,20 +1,36 @@
 <?php
-    $kmMaximoPermtido = 100;
-    $velocidadeVeiculo = 180;
+    $kmMaximoPermtido = 60;
+    $velocidadeVeiculo = 75;
 
 
-    if($velocidadeVeiculo >= $kmMaximoPermtido){
-        //leva muta
-        $margem = 0.07;
-        $velocidadeVeiculoComMargem = ($velocidadeVeiculo *  $margem) + $velocidadeVeiculo;
-        if($velocidadeVeiculoComMargem <= $kmMaximoPermtido ){
-           echo "Você foi mutado, velocidade permitida: KM {$velocidadeVeiculoComMargem}\nSua velocidade estava:  KM {$velocidadeVeiculo}"; 
+    if($velocidadeVeiculo >= 100){
+        // maior que 100km
+        // margem de erro de 7%
+        $margem = 0.07; // 7% na velocidade maxima
+        // calculo da velocidade com margem
+        $velocidadeVeiculoComMargem = ($kmMaximoPermtido *  $margem) + $kmMaximoPermtido;
+
+        if($velocidadeVeiculo >= $velocidadeVeiculoComMargem ){
+
+            //gravidade da multa
+            $marge20 = ($kmMaximoPermtido * 0.2) + $kmMaximoPermtido;
+            echo "Você foi mutado, velocidade permitida: KM {$velocidadeVeiculoComMargem}\nSua velocidade estava:  KM {$velocidadeVeiculo}"; 
         }else{
            echo "Você não foi mutado, velocidade permitida: KM {$velocidadeVeiculoComMargem}\nSua velocidade estava:  KM {$velocidadeVeiculo}";         
         }
     }else{
-        //nao leva muta
-         echo "Você não foi mutado, velocidade permitida: KM {$kmMaximoPermtido}\nSua velocidade estava:  KM {$velocidadeVeiculo}";
+        //menor que 100km 
+        // margem de erro de 7km
+        $margemKm = 7; //  7 km
+        $velocidadeVeiculoComMargemKm = $kmMaximoPermtido +$margemKm;
+
+        if($velocidadeVeiculo >= $velocidadeVeiculoComMargemKm ){
+            // mutado
+            echo "Você foi mutado, velocidade permitida: KM {$velocidadeVeiculoComMargemKm}\nSua velocidade estava:  KM {$velocidadeVeiculo}"; 
+        }else{
+            // nao levou muta
+             echo "Você não foi mutado, velocidade permitida: KM {$velocidadeVeiculoComMargemKm}\nSua velocidade estava:  KM {$velocidadeVeiculo}";
+        }      
     }
     
 ?>
